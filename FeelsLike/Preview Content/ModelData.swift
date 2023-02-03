1344edb01aa7a9f76763511a890dd4723e6a9bda
Ikbal Demirdoven <ikbaldemirdoven@icloud.com>
2023-02-02 23:24:48 -0800

    cleared the project

---
+++ FeelsLike/Preview Content/ModelData.swift
@@ -0,0 +1,8 @@
+//
+//  ModelData.swift
+//  FeelsLike
+//
+//  Created by Ikbal Demirdoven on 2023-02-02.
+//
+
+import Foundation
---
+++ FeelsLike/Views/WeatherView.swift
@@ -0,0 +1,21 @@
+//
+//  WeatherView.swift
+//  FeelsLike
+//
+//  Created by Ikbal Demirdoven on 2023-02-01.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    var weather : ResponseBody
+    var body: some View {
+        
+    }
+}
+
+struct WeatherView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherView(weather: ResponseBody)
+    }
+}
